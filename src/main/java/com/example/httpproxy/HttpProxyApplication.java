8f50fe5407bb093fd8f1a3dd4f9d6adb3c2c92fb
hanjun <hanjun>
2020-09-24 17:41:14 +0800

    代理

--- src/main/java/com/example/httpproxy/HttpProxyApplication.java
+++ src/main/java/com/example/httpproxy/HttpProxyApplication.java
@@ -1,13 +1,17 @@
 package com.example.httpproxy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HttpProxyApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(HttpProxyApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(HttpProxyApplication.class, args);
+        logger.error("代理启动成功");
     }
 
 }
